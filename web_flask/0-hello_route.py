80d07042b6f311db2853de99b489185035935b57
mmarzoo <marzouk12378@gmail.com>
2024-05-18 08:01:30 -0700

    adding files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,7 @@
 """
 start Flask application
 """
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,8 +10,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-     return 'Hello HBNB!'
+    return 'Hello HBNB!'
 
 
- if __name__ == '__main__':
-     app.run(host='0.0.0.0', port='5000')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
