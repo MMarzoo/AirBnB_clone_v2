b98da1fcbfefb3488da705887c1df2cbb3a5065f
mmarzoo <marzouk12378@gmail.com>
2024-05-18 08:27:31 -0700

    adding files

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,6 +2,7 @@
 """
 start Flask application
 """
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -21,7 +22,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
-    return 'C' + text.replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
